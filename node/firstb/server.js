23e1f58c849f4c97f9ed84d82942988207a98fb1
pradeeppaswan902-crypto <pradeeppaswan902@gmail.com>
2026-01-09 17:48:33 +0530

    UPDATE

--- node/firstb/server.js
+++ node/firstb/server.js
@@ -1,16 +1,17 @@
 import dotenv from 'dotenv'
-dotenv.config();
-import express, { json } from 'express'
+dotenv.config()
 
-const app = express();
-app.get("/",(req, res)=>{
-    console.log("Server Is Running")
-    res.json({Massage : "Server is Runing Suceessfully"})
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res)=>{
+    console.log("Server Started From Server")
+    res.json({Massage : "Get Started"})
 })
 
-const port = process.env.PORT || 5000;
-app.listen(port,()=>{
-    
-    console.log("Server Started At Port",port)
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+    console.log("Tumhara Server satart ho gya hai")
 })
- 
--- node/test1/index.js
+++ node/test1/index.js
@@ -1,17 +1,68 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
-import express from 'express'
+import express from 'express';
+const app = express();
 
-const app = express()
 
-app.get("/", (req, res)=>{
-    console.log("server Started")
-    res.json({Massage : "Working"})
+app.get("/", (req, res) => {
+ 
+  const data = {
+    email: "email@example.com",
+    pass: "Owla123"
+  };
+
+  
+  res.json(data);
+});
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+import {react, useState} from 'react'
+
+const App = ()=>{
+const [form , setForm] = useState({
+    UserName : "",
+    email : "",
+
 })
+const handaleChal = (e)=>{
+    const {name, value} = e.target;
+    setForm((pre)=>({
+        ...pre,
+        [name]: value
+    }))
+}
+const HandaleSubmit = (e) =>{
+    e.preventDefault();
+ 
+}
 
-const port = process.env.PORT || 5000
+return(
+    <>
+       <div>
+          <form className='border border:focuse:red  ' onSubmit={HandaleSubmit} >
+              <div>
+                <label>UserName</label>
+                <input type='text' name='UserName' value={form.UserName} onChange={handaleChal} />
+              </div>
+              <div>
+                <label>Email</label>
+                <input type='email' name='email' value={form.email} onChange={handaleChal} />
+              </div>
+              <div >
+                <button type='submit' className=''>Submit</button>
+              </div>
+          </form>
+       </div>
+    
+    </>
+)
 
-app.listen(port, ()=>{
-    console.log("started Port at",port)
-})
+
+}
